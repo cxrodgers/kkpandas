c33d024a9558eda8c5352961e028e28070f61e1c
Chris Rodgers <chris.rodgers@berkeley.edu>
2012-06-25 00:26:54 -0700

    bringing main structures into top-level

--- __init__.py
+++ __init__.py
@@ -1,6 +1,10 @@
 # klustakwik-loading functions .. probably change this filename
 from kkpandas import *
 
+# These are the main data structures (should probably be moved to base.py)
+from analysis import Folded, Binned
+
+# Bring in the other files
 import analysis
 import io
 import chris
