e3e5acec161415b4a99a38ec3e79abf4b54728f1
Chris Rodgers <chris.rodgers@berkeley.edu>
2012-06-25 02:41:20 -0700

    plotting functions for binned

--- plotting.py
+++ plotting.py
@@ -1,6 +1,36 @@
+import matplotlib.pyplot as plt
 
 
-# PLOTTING stuff
+def plot_binned_by_level(binned, ax=None):
+    if ax is None:
+        f = plt.figure()
+        ax = f.add_subplot(111)
+    
+    topkeys = list(binned.columns.levels[0])
+    lsl = ['-', ':', '.-']
+    for n, topkey in enumerate(topkeys):
+        plot_binned(binned[topkey], ax=ax, ls=lsl[n])
+    
+    ax.legend(binned.columns, loc='best')
+    plt.show()
+    return ax
+
+def plot_binned(binned, ax=None, **kwargs):
+    if ax is None:
+        f = plt.figure()
+        ax = f.add_subplot(111)
+    
+    csl = ['b', 'g', 'r', 'c', 'm', 'y', 'k', 'gray']
+    for n, (label, col) in enumerate(binned.rate.iteritems()):
+        if 'color' in kwargs:
+            ax.plot(binned.t, col.values, **kwargs)
+        else:
+            ax.plot(binned.t, col.values, color=csl[n], **kwargs)
+    
+    ax.legend(binned.columns, loc='best')
+    plt.show()
+    return ax
+
 def plot_psth_with_rasters_from_dict(
     dfolded, keys=None, spshape=None, bins=None):
     
