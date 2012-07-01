beff256ae082d943c77112311b44d917676c9cb2
Chris Rodgers <chris.rodgers@berkeley.edu>
2012-06-30 20:33:54 -0700

    brekaing pickers out of pipeline

--- chris.py
+++ chris.py
@@ -1,9 +1,12 @@
 """Wrapper and convenience methods tailored to my experiments"""
 
 import pandas
+import io
 import numpy as np
 import analysis
 import LBPB
+from ns5_process.RS_Sync import RS_Syncer
+from analysis import Folded
 
 # TRIALS_INFO picking functions with my defaults
 def pick_trial_numbers(trials_info, outcome='hit', nonrandom=0, 
@@ -48,57 +51,73 @@ def names2multilevel(df):
 
 
 
+class BlockTrialPicker:
+    """Given trials_info, returns (label, trial_numbers) for blocks"""
+    @classmethod
+    def pick(self, trials_info):
+        keys = ('LB', 'PB')
+        kwargs_l = ({'block': 2}, {'block': 4})
+        meth = pick_trial_numbers
+        
+        res = []
+        for key, kwargs in zip(keys, kwargs_l):
+            res.append((key, meth(trials_info, isnotnull=None, **kwargs)))
+        return res
+
+class EventTimePicker:
+    """Given event name and folded events_info, returns times to lock on"""
+    @classmethod
+    def pick(self, event_name, trials_l):
+        res = []
+        for trial in trials_l:
+            val = analysis.panda_pick_data(trial, event=event_name).time.item()
+            res.append(val)
+        return res
+
 # something like this should be the analysis pipeline
-def pipeline(unit):
-    # Find data
-    subdir = os.path.join(data_dir, unit.xpath('../../../@session_name')[0])
-    rs = RecordingSession.RecordingSession(subdir)
-    basename = rs.last_klusters_dir()
-    
-    # This could be put into a KK_server object
-    # You pass session name, group, and unit (or fully specified unit)
-    # It returns spikes and deals with memoization, flushing, etc
-    unum = int(unit.find('group').text) * 100 + int(unit.find('cluster').text)
-    spikes = kkpandas.from_KK(basename=basename, load_memoized=True,
-        save_memoized=True, group_multiplier=100, fs=30e3)
-    
-    # Spike selection
-    sspikes = np.asarray(spikes[spikes.unit == unum].spike_time)
+def pipeline_overblock_oneevent(kkserver, session, unit, rs,
+    trial_picker=BlockTrialPicker,
+    evname='play_stimulus_in'):
     
     # And a trial_server object?
-    trials_info = kkpandas.io.load_trials_info(rs.full_path)
-    events = kkpandas.io.load_events(rs.full_path)
+    trials_info = io.load_trials_info(rs.full_path)
+    events = io.load_events(rs.full_path)
+
+    # Spike selection
+    spikes = np.asarray(
+        kkserver.load(session=session, unit=unit).spike_time)
+
     
     # Select trials from behavior
-    LB_trial_numbers = kkpandas.chris.pick_trial_numbers(trials_info,
-        isnotnull=None, block=2)
-    PB_trial_numbers = kkpandas.chris.pick_trial_numbers(trials_info,
-        isnotnull=None, block=4)
+    picked_trials_l = BlockTrialPicker.pick(trials_info)
     
     # Fold events structure on start times
     rss = RS_Syncer(rs)
-    f = kkpandas.Folded.from_flat(flat=events, starts=rss.trialstart_nbase, 
+    f = Folded.from_flat(flat=events, starts=rss.trialstart_nbase, 
         subtract_off_center=False)
     
     # Convert to dict Folded representation with trial numbers as labels
     tn2ev = dict(zip(rss.btrial_numbers, f))
     
-    # Extract trials by number (put this as accessor method in Folded
-    LB_trials = [tn2ev[tn] for tn in LB_trial_numbers] # go in folded
-    PB_trials = [tn2ev[tn] for tn in PB_trial_numbers] # go in folded
+    # Take advantage of the fact that trial numbers are the link between
+    # picked_trials_l and tn2ev to
+    # Iterate over picked_trials_l and extract time from each trial
+    label2timelocks = {}
+    for label, trial_numbers in picked_trials_l:
+        # Extract trials by number (put this as accessor method in Folded
+        trials = [tn2ev[tn] for tn in trial_numbers] # go in folded
     
-    # Get timelock times by applying a function to each entry (put this in Folded)
-    LB_times = map(lambda ev: kkpandas.analysis.panda_pick_data(
-        ev, event='play_stimulus_in').time.item(),
-        LB_trials)
-    PB_times = map(lambda ev: kkpandas.analysis.panda_pick_data(
-        ev, event='play_stimulus_in').time.item(),
-        PB_trials)
-
-    # Now fold spike times on timelock times
-    d = {}
-    for label, times in zip(['LB', 'PB'], [LB_times, PB_times]):
-        d[label] = kkpandas.Folded.from_flat(
-            flat=sspikes, centers=times, dstart=-.25,
+        # Get timelock times by applying a function to each entry (put this in Folded)
+        times = EventTimePicker.pick(evname, trials)
+        
+        label2timelocks[labe] = times
+    
+    # Now fold out over timelocked times
+    res = {}
+    for label, timelocks in label2timelocks.items():
+        # Now fold spike times on timelock times    
+        res[label] = Folded.from_flat(
+            flat=spikes, centers=timelocks, dstart=-.25,
             dstop=.25)
-    ures[(rs.session_name, unum)] = d    
+
+    return res
